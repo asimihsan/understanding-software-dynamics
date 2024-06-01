adb68c4b37cee72c0a9df8a291d2b2fd3e401cbc
Asim Ihsan <ssh@sshemail.ihsan.io>
2024-05-31 21:57:56 -0400

    Fix duplicate variable declaration in main function.

--- src/mystery0.c
+++ src/mystery0.c
@@ -5,13 +5,14 @@
 #if defined(__x86_64__) || defined(_M_X64)
 #include <x86intrin.h>
 #elif defined(__aarch64__)
+
 #include <sys/time.h>
+
 #endif
 
 static const int kIterations = 1000 * 1000000;
 
 int main(int argc, const char **argv) {
-    uint64_t startcy = 0, stopcy = 0;
     uint64_t startcy = 0, stopcy = 0;
     uint64_t sum = 0;
 
