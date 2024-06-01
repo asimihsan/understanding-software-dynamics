bd9ff0ef72ae4c4e8231a5f637a933b7824d8aab
Asim Ihsan <ssh@sshemail.ihsan.io>
2024-05-31 22:41:21 -0400

    wip

--- src/mystery0.cpp
+++ src/mystery0.cpp
@@ -20,8 +20,11 @@ int main(int argc, const char **argv) {
 
     collector.start(); // Start collecting performance events
 
-    for (int i = 0; i < kIterations; ++i) {   // loop kIterations times
+    for (int i = 0; i < kIterations; i += 1) {   // loop kIterations times
         sum += 1;                               // the add we want to measure
+//        sum += 1;                               // the add we want to measure
+//        sum += 1;                               // the add we want to measure
+//        sum += 1;                               // the add we want to measure
     }
 
     event_count result = collector.end(); // End collecting performance events
@@ -35,7 +38,17 @@ int main(int argc, const char **argv) {
     printf("Cycles: %lu\n", result.cycles());
     printf("Branches: %lu\n", result.branches());
     printf("Branch misses: %lu\n", result.branch_misses());
-    printf("Cycles per iteration: %f\n", cycles_per_iteration);
+    printf("Cycles per loop: %f\n", cycles_per_iteration);
+//    printf("Cycles per add: %f\n", cycles_per_iteration / 4.0);
+    printf("IPC: %f\n", result.instructions() / (double) result.cycles());
+    printf("Branch prediction miss rate: %f\n",
+           result.branch_misses() / (double) result.branches());
+    printf("Branch prediction hit rate: %f\n",
+           1.0 - result.branch_misses() / (double) result.branches());
+    printf("Instructions per branch miss: %f\n",
+           result.instructions() / (double) result.branch_misses());
+    printf("Instructions per branch hit: %f\n",
+           result.instructions() / (double) (result.branches() - result.branch_misses()));
 
     return 0;
 }
