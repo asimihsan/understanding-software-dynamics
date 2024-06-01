8233b395104e7e89a635d446c07992c9e0d166c7
Asim Ihsan <ssh@sshemail.ihsan.io>
2024-05-31 21:55:07 -0400

    Refactored code to measure cycles per iteration in mystery0.c.

--- src/mystery0.c
+++ src/mystery0.c
@@ -1,6 +1,21 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <time.h>
+#include <x86intrin.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+static const int kIterations = 1000 * 1000000;
+
+int main(int argc, const char **argv) {
+    uint64_t startcy, stopcy;
+    uint64_t sum = 0;
+    startcy = __rdtsc();                       // starting cycle count
+    for (int i = 0; i < kIterations; ++i) {   // loop kIterations times
+        sum += 1;                               // the add we want to measure
+    }
+    stopcy = __rdtsc();                       // ending cycle count
+    int64_t elapsed = stopcy - startcy;
+    double felapsed = elapsed;
+    fprintf(stdout, "%d iterations, %lu cycles, %4.2f cycles/iteration\n",
+            kIterations, elapsed, felapsed / kIterations);
     return 0;
 }
